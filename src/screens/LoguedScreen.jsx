09c93b013529beecf8f1c34d3b53e45e2c13e274
tchomysz <tomasteodoro.chomyszyn@everis.nttdata.com>
2022-09-30 17:02:21 -0300

    LoguedScreen, styles,

--- src/screens/LoguedScreen.jsx
+++ src/screens/LoguedScreen.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
-import { Image, Text, View } from 'react-native'
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import { styles } from '../theme/styles'
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 export const LoguedScreen = () => {
   return (
     <View>
-        <View style={{ flexDirection: 'row' }}>
+        <View style={ styles.loguedScreenHeader }>
           <Image
               source= { require('../assets/TempLogo.png') }
+              style={{ width: 60, height: 60 }}
           />
-          <Text style={styles.title}>TravelOn</Text>
-          <Ionicons
-            name= 'heart-outline'
-            size={ 35 }
-            color= 'black'
-          />
+          <Text style={{ color: 'black', fontSize: 18, fontWeight: 'bold' }}>TravelOn</Text>
+          <TouchableOpacity>
+            <Ionicons
+              name= 'heart-outline'
+              size={ 35 }
+              color= 'black'
+            />
+          </TouchableOpacity>
         </View>
+        <ScrollView>
+          
+        </ScrollView>
     </View>
   )
 }
