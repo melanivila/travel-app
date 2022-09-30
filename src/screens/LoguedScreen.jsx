168127d7af3a987ec1ce3e99303ac14c1d01c524
tchomysz <tomasteodoro.chomyszyn@everis.nttdata.com>
2022-09-30 16:17:45 -0300

    LoguedScreen y Temp logo

---
+++ src/screens/LoguedScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Image, View } from 'react-native'
+
+export const LoguedScreen = () => {
+  return (
+    <View>
+        <Image
+            source= { require('../assets/TempLogo.png') }
+        />
+    </View>
+  )
+}
