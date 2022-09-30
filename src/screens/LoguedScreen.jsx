4f2b6ef89fe5501bb9b38fbaba9842091f698b4c
tchomysz <tomasteodoro.chomyszyn@everis.nttdata.com>
2022-09-30 16:32:09 -0300

    LoguedScreen

--- src/screens/LoguedScreen.jsx
+++ src/screens/LoguedScreen.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import { Image, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
+import { styles } from '../theme/styles'
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export const LoguedScreen = () => {
   return (
     <View>
-        <Image
-            source= { require('../assets/TempLogo.png') }
-        />
+        <View style={{ flexDirection: 'row' }}>
+          <Image
+              source= { require('../assets/TempLogo.png') }
+          />
+          <Text style={styles.title}>TravelOn</Text>
+          <Ionicons
+            name= 'heart-outline'
+            size={ 35 }
+            color= 'black'
+          />
+        </View>
     </View>
   )
 }
